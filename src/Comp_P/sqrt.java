ab2e24b6072c35bd4b75d91cfaa9bfec99d7e70e
ANIKET7419 <aniketranag123@gmail.com>
2020-06-12 14:11:06 +0530

    Competetive Programming

---
+++ src/Comp_P/sqrt.java
@@ -0,0 +1,33 @@
+package Comp_P;
+
+import java.util.Scanner;
+
+class Solution_sqrt {
+    public int mySqrt(int x) {
+        double start=0,end=((double)x/2)+1;
+        for(int i=0;i<x;i++)
+
+        {
+            double mid = (start + end) / 2;
+            if (mid * mid - x >= 0) {
+                end = mid;
+            }
+            else {
+                start = mid;
+            }
+
+        }
+        return (int)end;
+
+    }
+}
+public class sqrt {
+    public static void main(String[] args) {
+        Solution_sqrt object=new Solution_sqrt();
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt();
+        System.out.println(object.mySqrt(x));
+    }
+
+
+}
