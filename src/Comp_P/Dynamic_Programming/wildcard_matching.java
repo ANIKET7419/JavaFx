4770fb60b2c9a13ccf7c7a03b84da5416b0235bb
ANIKET7419 <aniketranag123@gmail.com>
2020-06-14 13:40:47 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/wildcard_matching.java
+++ src/Comp_P/Dynamic_Programming/wildcard_matching.java
@@ -6,36 +6,94 @@ class handler
 {
     String data;
     String pattern;
+    byte storage[][];
     void input(String data,String pattern)
     {
         this.data=data;
         this.pattern=pattern;
+        storage=new byte[this.pattern.length()+1][this.data.length()+1];
+        for (int i=0;i<this.pattern.length()+1;i++)
+        {
+            for (int j=0;j<this.data.length()+1;j++)
+            {
+                storage[i][j]=-1;
+            }
+        }
     }
     boolean ismatched(int i,int j) {
-        if (i == 0 || j == 0) {
-            if (i == 0 && j == 0)
+        if(storage[i][j]!=-1)
+        {
+            if(storage[i][j]==1)
                 return true;
+            else
+                return false;
+        }
+        if (i == 0 || j == 0) {
+            if (i == 0 && j == 0) {
+                storage[i][j]=1;
+                return true;
+            }
             else {
-                if (i == 0)
+                if (i == 0) {
+                    storage[i][j]=0;
                     return false;
+                }
                 else {
                     if (pattern.charAt(i - 1) == '*') {
-                        return ismatched(i - 1, j);
+                       if(ismatched(i-1,j))
+                       {
+                           storage[i][j]=1;
+                           return true;
+                       }
+                       else
+                       {
+                           storage[i][j]=0;
+                           return false;
+                       }
                     }
                 }
             }
         } else {
             if (pattern.charAt(i - 1) != '*' && pattern.charAt(i - 1) != '?') {
                 if (data.charAt(j - 1) == pattern.charAt(i - 1)) {
-                    return ismatched(i - 1, j - 1);
+                    if(ismatched(i-1,j-1))
+                    {
+                        storage[i][j]=1;
+                        return true;
+                    }
+                    else
+                    {
+                        storage[i][j]=0;
+                        return false;
+                    }
                 } else {
+                    storage[i][j]=0;
                     return false;
                 }
             } else {
                 if (pattern.charAt(i - 1) == '?') {
-                    return ismatched(i - 1, j - 1);
+                    if(ismatched(i-1,j-1))
+                    {
+                        storage[i][j]=1;
+                        return true;
+                    }
+                    else
+                    {
+                        storage[i][j]=0;
+                        return false;
+                    }
                 } else {
-                    return ismatched(i - 1, j) || ismatched(i, j - 1);
+
+                    if( ismatched(i - 1, j) || ismatched(i, j - 1))
+                    {
+                        storage[i][j]=1;
+                        return true;
+                    }
+                    else
+                    {
+                        storage[i][j]=0;
+                        return false;
+                    }
                 }
 
 
