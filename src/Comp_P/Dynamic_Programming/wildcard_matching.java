125ff3ecdc450ffaba6ecddc79b0e02a73319e1a
ANIKET7419 <aniketranag123@gmail.com>
2020-06-14 13:08:40 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/wildcard_matching.java
+++ src/Comp_P/Dynamic_Programming/wildcard_matching.java
@@ -11,48 +11,40 @@ class handler
         this.data=data;
         this.pattern=pattern;
     }
-    boolean ismatched(int i,int j)
-    {
-        if(i==0||j==0)
-        {
-         if(i==0&&j==0)
-             return true;
-         else
-         {
-             if (i==0)
-                 return false;
-             else if (j==0)
-             {
-                 if(pattern.charAt(i-1)=='*')
-                 {
-                     return ismatched(i-1,j);
-                 }
-             }
-         }
-        }
-        if(pattern.charAt(i-1)!='*'&&pattern.charAt(i-1)!='?') {
-            if (data.charAt(j-1) == pattern.charAt(i-1)) {
-                return ismatched(i - 1, j - 1);
+    boolean ismatched(int i,int j) {
+        if (i == 0 || j == 0) {
+            if (i == 0 && j == 0)
+                return true;
+            else {
+                if (i == 0)
+                    return false;
+                else {
+                    if (pattern.charAt(i - 1) == '*') {
+                        return ismatched(i - 1, j);
+                    }
+                }
+            }
+        } else {
+            if (pattern.charAt(i - 1) != '*' && pattern.charAt(i - 1) != '?') {
+                if (data.charAt(j - 1) == pattern.charAt(i - 1)) {
+                    return ismatched(i - 1, j - 1);
+                } else {
+                    return false;
+                }
             } else {
-                return false;
-            }
-        }
-        else
-        {
-            if(pattern.charAt(i-1)=='?')
-            {
-                return ismatched(i - 1, j - 1);
-            }
-            else
-            {
-                return ismatched(i - 1, j) || ismatched(i, j - 1);
-            }
+                if (pattern.charAt(i - 1) == '?') {
+                    return ismatched(i - 1, j - 1);
+                } else {
+                    return ismatched(i - 1, j) || ismatched(i, j - 1);
+                }
 
 
+            }
 
         }
-
+        return false;
     }
+
 }
 public class wildcard_matching {
 
