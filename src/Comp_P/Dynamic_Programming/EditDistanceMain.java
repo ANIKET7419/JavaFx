454c13e09b91c2d87814d8c4cdc0ce54d786c515
ANIKET7419 <aniketranag123@gmail.com>
2020-06-15 11:05:32 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/EditDistanceMain.java
+++ src/Comp_P/Dynamic_Programming/EditDistanceMain.java
@@ -5,27 +5,45 @@ import java.util.Scanner;
 class EditDistance
 {
     String word1,word2;
+    int storage[][];
     EditDistance(String word1, String word2)
     {
         this.word1=word1;
         this.word2=word2;
+        storage=new int[word1.length()+1][word2.length()+1];
+        for(int i=0;i<word1.length()+1;i++)
+        {
+            for (int j=0;j<word2.length()+1;j++)
+            {
+                storage[i][j]=-1;
+            }
+        }
     }
     int totalDistance(int i,int j)
     {
         if(i==0||j==0)
         { if(i==j)
-            { return 0; }
+            {
+                storage[i][j]=0;
+                return 0; }
             else
             { if(i==0)
-                { return j; }
-                else return i;}}
+                { storage[i][j]=j;
+                    return j; }
+                else{
+                    storage[i][j]=i;
+                    return i;}}}
+        if(storage[i][j]!=-1)
+            return storage[i][j];
         if(word1.charAt(i-1)==word2.charAt(j-1))
         {
-            return totalDistance(i-1,j-1);
+            storage[i][j]= totalDistance(i-1,j-1);
+            return storage[i][j];
         }
         else
         {
-            return Math.min(totalDistance(i-1,j-1),Math.min(totalDistance(i-1,j),totalDistance(i,j-1)))+1;
+            storage[i][j]= Math.min(totalDistance(i-1,j-1),Math.min(totalDistance(i-1,j),totalDistance(i,j-1)))+1;
+            return storage[i][j];
         }
 
     }
