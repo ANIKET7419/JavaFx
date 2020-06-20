2c1accb3da9d46d36bb97858a8788bd488668974
ANIKET7419 <aniketranag123@gmail.com>
2020-06-20 16:16:54 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/LCS_FORMAT/LongestCommonSubsequence.java
+++ src/Comp_P/Dynamic_Programming/LCS_FORMAT/LongestCommonSubsequence.java
@@ -1,28 +1,39 @@
 package Comp_P.Dynamic_Programming.LCS_FORMAT;
 
 import java.net.SecureCacheResponse;
+import java.util.Arrays;
 import java.util.Scanner;
 
 class LCSHandler
 {
     String s1,s2;
+    int storage[][];
     void input(String s1,String s2)
     {
         this.s1=s1;
         this.s2=s2;
+        storage=new int[s1.length()+1][s2.length()+1];
+        for (int temp[]:storage)
+        {
+            Arrays.fill(temp,-1);
+        }
     }
     int lcs(int i,int j)
     {
         if(i==0||j==0)
             return 0;
-
+        if(storage[i][j]!=-1)
+            return storage[i][j];
         if(s1.charAt(i-1)==s2.charAt(j-1))
         {
-        return lcs(i-1,j-1)+1;
+
+        storage[i][j]= lcs(i-1,j-1)+1;
+        return storage[i][j];
         }
         else
         {
-            return Math.max(lcs(i-1,j),lcs(i,j-1));
+            storage[i][j]= Math.max(lcs(i-1,j),lcs(i,j-1));
+            return storage[i][j];
         }
     }
 
