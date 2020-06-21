efb4b5999ea63076fbfd4b2e9dab01bb35df0433
ANIKET7419 <aniketranag123@gmail.com>
2020-06-21 18:47:23 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/JumpGame.java
+++ src/Comp_P/Dynamic_Programming/JumpGame.java
@@ -1,57 +1,46 @@
 package Comp_P.Dynamic_Programming;
 
-import javax.swing.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 class JumpGameHandler
 {
     int data[];
-    int storage[][];
+    int storage[];
     void input(int data[])
     {
         this.data=data;
-        storage=new int[data.length][data.length];
-        for (int temp[]:storage)
-        {
-            Arrays.fill(temp,-1);
-        }
+        storage=new int[data.length];
+        Arrays.fill(storage,-1);
     }
     int minimumjump()
     {
-        int minimum_jump=0;
-        for (int i=0;i<data.length;i++)
+
+        storage[0]=0;
+        for (int i =1;i<data.length;i++)
         {
-            for (int j=0;j<data.length;j++)
+            int min=Integer.MAX_VALUE;
+            for (int j=i-1;j>=0;j--)
             {
-                if(i==j)
-                    storage[i][j]=0;
-                else if(i>j)
-                    storage[i][j]=-1;
-                else {
-                    if(data[i]+i>=j)
-                        storage[i][j]=storage[0][j]+1;
-                    else
+                if(data[j]+j>=i)
+                {
+                    if(min>storage[j])
                     {
-                        if(data[j-1]+j-1>=j)
-                        storage[i][j]=storage[0][j-1]+1;
-                        else
-                            storage[i][j]=-1;
+                        min=storage[j];
                     }
+                }
+                if(min==Integer.MAX_VALUE)
+                {
+                    storage[i]=0;
+                }
+                else {
+                    storage[i]=min+1;
                 }
             }
         }
-        for (int i=0;i<data.length;i++)
-        {
-            for (int j=0;j<data.length;j++)
-            {
-                System.out.print(storage[i][j]+" ");
-            }
-            System.out.println();
-        }
 
 
-        return minimum_jump;
+        return storage[data.length-1];
     }
 }
 public class JumpGame {
