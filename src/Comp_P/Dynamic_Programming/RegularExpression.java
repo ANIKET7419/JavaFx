6f3fc5348cf995bcf1f1c554370264b3f212611f
ANIKET7419 <aniketranag123@gmail.com>
2020-06-22 15:44:35 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/RegularExpression.java
+++ src/Comp_P/Dynamic_Programming/RegularExpression.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 class RegularExpressionHandler
 {
     String word,pattern;
+    boolean evaluate(int i)
+    {
+        StringBuilder builder=new StringBuilder(pattern.substring(0,i));
+        for (int j=0;j<i;j++)
+        {
+            if(builder.charAt(j)=='*')
+            {
+                builder.deleteCharAt(j);
+                if(j!=0)
+                builder.deleteCharAt(j-1);
+            }
+        }
+        if(builder.length()!=0)
+            return false;
+        else
+            return true;
+    }
     void input(String word,String pattern)
     {
         this.pattern=pattern;
@@ -22,6 +39,10 @@ class RegularExpressionHandler
         }
         else  if (i==0&&j!=0)
             return false;
+        else if(j==0&&i!=0)
+        {
+           return evaluate(i);
+        }
         else
         {
             if(pattern.charAt(i-1)!='.'&&pattern.charAt(i-1)!='*') {
@@ -40,11 +61,11 @@ class RegularExpressionHandler
             }
             else {
                 boolean formore=false;
-                if(pattern.charAt(i-2)==word.charAt(j-1))
+                if(pattern.charAt(i-2)==word.charAt(j-1)&&isMatched(i,j-1))
                 {
                     formore=true;
                 }
-                else if (pattern.charAt(i-2)=='.')
+                else if (pattern.charAt(i-2)=='.'&&isMatched(i,j-1))
                     formore=true;
                 return  isMatched(i-1,j)||isMatched(i-2,j)|| formore;
             }
@@ -64,5 +85,6 @@ public class RegularExpression {
         pattern=scanner.nextLine();
         handler.input(word,pattern);
         System.out.println("Is Matched "+handler.isMatched(pattern.length(),word.length()));
+
     }
 }
