9060ecba0c8643b22a83aed08e8afc5412ac51aa
ANIKET7419 <aniketranag123@gmail.com>
2020-06-22 17:19:46 +0530

    Competetive Programming

---
+++ src/Comp_P/IntegerToRoman.java
@@ -0,0 +1,18 @@
+package Comp_P;
+
+class ITRHandler
+{
+    int integer;
+    void  input(int integer)
+    {
+        this.integer=integer;
+    }
+
+
+}
+public class IntegerToRoman {
+    public static void main(String[] args) {
+        int a;
+
+    }
+}
