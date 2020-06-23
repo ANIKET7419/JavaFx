dcfdb8d4dfe278074632c23ab3dcf8a10d622fc1
ANIKET7419 <aniketranag123@gmail.com>
2020-06-23 14:45:36 +0530

    Competetive Programming

--- src/Comp_P/Window_Sliding/MinimumWindowSubstring.java
+++ src/Comp_P/Window_Sliding/MinimumWindowSubstring.java
@@ -22,7 +22,7 @@ import java.util.Scanner;
 class MinimumWindowSubstringHandler
 {
     String s1,s2;
-    void input(String s,String s2)
+    void input(String s1,String s2)
     {
         this.s1=s1;
         this.s2=s2;
