22a00f9f8c487380aa8403ee5dc8f3de31a27b67
ANIKET7419 <aniketranag123@gmail.com>
2020-06-25 15:46:39 +0530

    Competetive Programming

---
+++ src/Comp_P/Dynamic_Programming/RaceCar.java
@@ -0,0 +1,57 @@
+package Comp_P.Dynamic_Programming;
+
+import java.net.SecureCacheResponse;
+import java.util.Scanner;
+
+class RaceCarHandler
+{
+    int target;
+    String result="";
+    void input(int target)
+    {
+        this.target=target;
+    }
+    void minimuminstruction(int position,int speed)
+    {
+        if (position==target)
+        {
+            System.out.println(result);
+            return;
+
+        }
+        else if (position<0)
+            return;
+        else if (position<target || position>target)
+        {
+           position+=speed;
+           speed*=2;
+           String temp=result;
+           result+="A";
+           minimuminstruction(position,speed);
+           result=temp;
+        }
+        if (target>position)
+        {
+            if (speed<0)
+                speed=1;
+            else
+            speed=-1;
+            String temp=result;
+            result+="R";
+            minimuminstruction(position,speed);
+            result=temp;
+        }
+
+    }
+}
+public class RaceCar {
+    public static void main(String[] args) {
+        RaceCarHandler handler=new RaceCarHandler();
+        int n;
+        System.out.println("Enter The Target ");
+        Scanner scanner=new Scanner(System.in);
+        n=scanner.nextInt();
+        handler.input(n);
+        handler.minimuminstruction(0,1);
+    }
+}
