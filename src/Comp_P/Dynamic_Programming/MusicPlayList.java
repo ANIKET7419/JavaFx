3e312dbe5fecfd05632e331cd48a7c59f7e358d8
ANIKET7419 <aniketranag123@gmail.com>
2020-06-26 18:53:56 +0530

    Competetive Programming

---
+++ src/Comp_P/Dynamic_Programming/MusicPlayList.java
@@ -0,0 +1,54 @@
+package Comp_P.Dynamic_Programming;
+
+import java.util.Scanner;
+
+class MusicPlayListHandler
+{
+    int N,K,L;
+    void input(int N,int K,int L)
+    {
+        this.N=N;
+        this.K=K;
+        this.L=L;
+    }
+    int totalways()
+    {
+        int ways=1;
+        int dp[]=new int[L+1];
+        dp[0]=0;
+        int tempN=N;
+        int KpositionerS=1;
+        dp[1]=tempN;
+        tempN--;
+        for (int i=2;i<L+1;i++)
+        {
+            if (i-KpositionerS==(K+1))
+            {
+                tempN+=1;
+            }
+            dp[i]=tempN;
+            tempN--;
+        }
+
+        for (int i=1;i<L;i++)
+        {
+            ways*=dp[i];
+        }
+        return ways;
+    }
+}
+public class MusicPlayList {
+    public static void main(String[] args) {
+        int N,K,L;
+        System.out.println("Enter  Different  number of songs ");
+        Scanner scanner=new Scanner(System.in);
+        N=scanner.nextInt();
+        System.out.println("Enter Playlist size");
+        L=scanner.nextInt();
+        System.out.println("Enter Value of K");
+        K=scanner.nextInt();
+        MusicPlayListHandler handler =new MusicPlayListHandler();
+        handler.input(N,K,L);
+        System.out.println("The Total  Ways "+handler.totalways());
+    }
+}
