a47be14688eca23f33677f6824590a4d90d03043
ANIKET7419 <aniketranag123@gmail.com>
2020-06-27 16:31:36 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/MusicPlayListDP.java
+++ src/Comp_P/Dynamic_Programming/MusicPlayListDP.java
@@ -4,6 +4,39 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 //This DP solution is not working yet please use only backtracking solution
+
+
+/*
+
+THis is dp approach actually useful
+
+class Solution {
+    public int numMusicPlaylists(int N, int L, int K) {
+        int MOD = 1_000_000_007;
+
+        long[][] dp = new long[L+1][N+1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= L; ++i)
+            for (int j = 1; j <= N; ++j) {
+                dp[i][j] += dp[i-1][j-1] * (N-j+1);
+                dp[i][j] += dp[i-1][j] * Math.max(j-K, 0);
+                dp[i][j] %= MOD;
+            }
+  for(long temp[]:dp)
+  {
+      for(long data:temp)
+      {
+          System.out.print(data+" ");
+      }
+      System.out.println();
+  }
+
+        return (int) dp[L][N];
+    }
+}
+
+
+ */
 class MusicPlayListHandlerDP
 {
     int N,K,L;
