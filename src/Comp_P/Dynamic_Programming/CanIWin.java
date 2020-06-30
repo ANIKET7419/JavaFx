7acceb44847a5317bdd8f57b945c0b68cd0a8c06
ANIKET7419 <aniketranag123@gmail.com>
2020-06-30 19:00:03 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/CanIWin.java
+++ src/Comp_P/Dynamic_Programming/CanIWin.java
@@ -1,26 +1,25 @@
 package Comp_P.Dynamic_Programming;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
+//Under Process
 class CanIWinHandler
 {
+    int maximum;
+    boolean istraversed[];
     byte[] caniwin(int i,int j,byte turn,int target)
     {
         byte[]result=new byte[]{0,1};
         if (i>j)
             return new byte[]{turn,0};
-        else if (i==j)
-        {
-            if (target<=i)
-                return new byte[]{turn,1};
-            else
-                return new byte[]{turn,0};
-        }
+        else if (target<=maximum&&!(istraversed[maximum]))
+            return new byte[]{turn,1};
         else {
 
 
             for (int k=i;k<=j;k++)
             {
+                istraversed[k]=true;
                 byte first[],second[];
                 if (turn==1) {
                     first=caniwin(i, k-1, (byte) 0,target-k );
@@ -30,7 +29,8 @@ class CanIWinHandler
                     first=caniwin(i, k-1, (byte) 1,target-k );
                     second=caniwin(k+1,j,(byte)1,target-k);
                 }
-                if ((first[0]==1&&first[1]==1)||(second[0]==1&&second[1]==1))
+                istraversed[k]=false;
+                if ((first[0]==1&&first[1]==1)&&(second[0]==1&&second[1]==1))
                 {
                     result[0]=1;
                     result[1]=1;
@@ -51,6 +51,9 @@ public class CanIWin {
         target=scanner.nextInt();
         System.out.println("Enter Maximum Value");
         maximumvalue=scanner.nextInt();
+        handler.maximum=maximumvalue;
+        handler.istraversed=new boolean[maximumvalue+1];
+        Arrays.fill(handler.istraversed,false);
         byte []result=handler.caniwin(1,maximumvalue,(byte)1,target);
         if (result[0]==1&&result[1]==1)
         {
