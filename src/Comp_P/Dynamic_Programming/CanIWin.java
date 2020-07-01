16ae6c940def0797f267096c886bc35111a1705c
ANIKET7419 <aniketranag123@gmail.com>
2020-07-01 17:01:07 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/CanIWin.java
+++ src/Comp_P/Dynamic_Programming/CanIWin.java
@@ -7,64 +7,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 class CanIWinHandler
 {
-    byte complement(byte value)
+
+    int target,maximum;
+    void input(int target,int maximum)
     {
-        if(value==0)
-            return 1;
-        else return 0;
+        this.target=target;
+        this.maximum=maximum;
     }
-    byte caniwin(int i,int j,int target)
+    boolean caniwin(int  state,int target)
     {
-
-        byte  result=0;
-        if (i>j)
-            return -1;
-        else if (i==j)
+        boolean result1=false;
+        for (int i=0;i<maximum;i++)
         {
-            if (target<=i)
-                return 1;
-            else
-                return 0;
-        }
-        else
-        {
-
-            int sum=0;
-            for (int k=i;k<=j;k++)
-                sum+=k;
-            if (sum<target)
-                return 0;
-
-            for (int k=i;k<=j;k++)
+            int currentstate=1<<i;
+            int result=currentstate & state;
+            if (result==0)
             {
-                if (target-k<=0)
-                {
-                    return 1;
-                }
-                else {
-                    byte temp1 = caniwin(i, k - 1, target - k);
-                    byte temp2 = caniwin(k + 1, j, target - k);
-                    if (temp1 != -1 && temp2 != -1) {
-                        if (temp1 == 0 && temp2 == 0) {
-                            result = 1;
-
-                        }
-                    } else if (temp1 == -1) {
-                        if (temp2 == 0) {
-                            result = 1;
-
-                        }
-                    } else {
-                        if (temp1 == 0) {
-                            result = 1;
-
-                        }
-                    }
-                }
+                if (target<=i+1)
+                    return true;
+               boolean temp=caniwin(currentstate|state,target-(i+1));
+               if (!temp)
+               {
+                   result1=true;
+               }
             }
-
         }
-        return result;
+        return result1;
+
     }
 }
 
@@ -78,6 +47,6 @@ public class CanIWin {
         target=scanner.nextInt();
         System.out.println("Enter Maximum Value");
         maximumvalue=scanner.nextInt();
-        System.out.println("Will I Win ? "+(handler.caniwin(1,maximumvalue,target)==1?true:false));
+        System.out.println("Will I Win ? "+handler.caniwin(0,target));
     }
 }
