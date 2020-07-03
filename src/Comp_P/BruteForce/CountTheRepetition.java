4520dee61fb80e70b13667d7961d6d6bef3f038d
ANIKET7419 <aniketranag123@gmail.com>
2020-07-03 15:35:55 +0530

    Competetive Programming

--- src/Comp_P/BruteForce/CountTheRepetition.java
+++ src/Comp_P/BruteForce/CountTheRepetition.java
@@ -1,6 +1,56 @@
 package Comp_P.BruteForce;
 //This  approach is not useful for long input there it may cause  stack overflow error because of recursion
 
+
+/*
+
+
+
+Better Brute Force Approach
+
+prefer https://leetcode.com/problems/count-the-repetitions/Figures/466/count_the_repititions.png for understanding
+
+class Solution
+{
+
+    public:
+
+    int getMaxRepetitions(string s1, int n1, string s2, int n2)
+{
+    if (n1 == 0)
+        return 0;
+    int indexr[s2.length() + 1]; // index at start of each s1 block
+        indexr[0]=0;
+    int countr[s2.length()+ 1]; // count of repititions till the present s1 block
+        countr[0]=0;
+    int index = 0, count = 0;
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < s1.size(); j++) {
+            if (s1[j] == s2[index])
+                ++index;
+            if (index == s2.size()) {
+                index = 0;
+                ++count;
+            }
+        }
+        countr[i] = count;
+        indexr[i] = index;
+        for (int k = 0; k < i; k++) {
+            if (indexr[k] == index) {
+                int prev_count = countr[k];
+                int pattern_count = (countr[i] - countr[k]) * ((n1  - k-1) / (i - k));
+                int remain_count = countr[k + (n1 - k-1) % (i - k)] - countr[k];
+                return (prev_count + pattern_count + remain_count) / n2;
+            }
+        }
+    }
+    return countr[n1 - 1] / n2;
+}};
+
+
+
+ */
+
 import java.util.Scanner;
 
 class CountTheRepetitionHandler
