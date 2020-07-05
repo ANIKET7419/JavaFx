b9faac6b499982d4cf3701c886f220f4a7977ea7
ANIKET7419 <aniketranag123@gmail.com>
2020-07-05 11:24:56 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/BuildMaximumNumber.java
+++ src/Comp_P/Dynamic_Programming/BuildMaximumNumber.java
@@ -1,6 +1,7 @@
 package Comp_P.Dynamic_Programming;
 import Comp_P.Reusable.SegmentTree;
 import java.util.Scanner;
+//Under Progress
 class BuildMaximumNumberHandler
 {
 
@@ -8,16 +9,94 @@ class BuildMaximumNumberHandler
     int result[];
     int k;
     int length;
+    boolean flag=false;
+    SegmentTree tree1,tree2;
      void input(int data1[],int data2[],int k)
      {
          this.data1=data1;
          this.data2=data2;
+         tree1=new SegmentTree();
+         tree1.input(data1);
+         tree1.construct();
+         tree2=new SegmentTree();
+         tree2.input(data2);
+         tree2.construct();
          result=new int[k];
          this.k=k;
          length=k;
+
      }
-     int[] maximumNumber()
+     void maximumNumber(int start1,int end1,int start2,int end2,int k)
      {
+
+         if (start1>end1||start2>end2)
+             return;
+         else   if ((end1-start1+1)+(end2-start2+1)<k)
+             return;
+         if (k==0)
+         {
+             flag=true;
+             return;
+         }
+
+         int maximum1[]=tree1.queryMax(start1,end1);
+         int maximum2[]=tree2.queryMax(start2,end2);
+        if (maximum1[0]>maximum1[0])
+        {
+            result[length-k]=maximum1[0];
+            maximumNumber(maximum1[1]+1,end1,start2,end2,k-1);
+            maximumNumber(start1,maximum1[1]-1,start2,end2,k);
+
+        }
+       else if (maximum1[0]==maximum2[0])
+       {
+           result[length-k]=maximum1[0];
+           maximumNumber(maximum1[1]+1,end1,start2,end2,k-1);
+           result[length-k]=maximum2[0];
+           maximumNumber(start1,end1,maximum2[0]+1,end2,k-1);
+
+       }
+       else {
+
+
+       }
+
+     }
+
+
+}
+public class BuildMaximumNumber {
+    public static void main(String[] args) {
+          BuildMaximumNumberHandler handler =new BuildMaximumNumberHandler();
+          int n1,n2;
+          System.out.println("Enter The length of 1st Array");
+          Scanner scanner=new Scanner( System.in);
+          n1=scanner.nextInt();
+          System.out.println("Enter the length of 2nd Array");
+          n2=scanner.nextInt();
+          System.out.println("Enter data for 1st ");
+          int data1[]=new int[n1];
+          int data2[]=new int[n2];
+          for (int i=0;i<n1;i++)
+              data1[i]=scanner.nextInt();
+          System.out.println("Enter the data for 2nd ");
+          for (int i=0;i<n2;i++)
+              data2[i]=scanner.nextInt();
+          System.out.println("Enter the value of k");
+          int k=scanner.nextInt();
+          handler.input(data1,data2,k);
+          //int result[]=handler.maximumNumber();
+          //for (int i=0;i<result.length;i++)
+           //   System.out.print(result[i]+"  , ");
+
+
+
+
+    }
+}
+/*
+
+
           if (data1.length+data2.length<k)
               return new int[]{};
           SegmentTree tree1=new SegmentTree();
@@ -98,38 +177,4 @@ class BuildMaximumNumberHandler
              }
          }
 
-
-      return result;
-     }
-
-
-}
-public class BuildMaximumNumber {
-    public static void main(String[] args) {
-          BuildMaximumNumberHandler handler =new BuildMaximumNumberHandler();
-          int n1,n2;
-          System.out.println("Enter The length of 1st Array");
-          Scanner scanner=new Scanner( System.in);
-          n1=scanner.nextInt();
-          System.out.println("Enter the length of 2nd Array");
-          n2=scanner.nextInt();
-          System.out.println("Enter data for 1st ");
-          int data1[]=new int[n1];
-          int data2[]=new int[n2];
-          for (int i=0;i<n1;i++)
-              data1[i]=scanner.nextInt();
-          System.out.println("Enter the dta for 2nd ");
-          for (int i=0;i<n2;i++)
-              data2[i]=scanner.nextInt();
-          System.out.println("Enter the value of k");
-          int k=scanner.nextInt();
-          handler.input(data1,data2,k);
-          int result[]=handler.maximumNumber();
-          for (int i=0;i<result.length;i++)
-              System.out.print(result[i]+"  , ");
-
-
-
-
-    }
-}
+ */
