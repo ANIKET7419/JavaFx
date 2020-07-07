9fea51d0946abddfe665e362ed2d901cdfbc0962
ANIKET7419 <aniketranag123@gmail.com>
2020-07-07 10:33:11 +0530

    Competetive Programming

--- src/Comp_P/Dynamic_Programming/DungeonGame.java
+++ src/Comp_P/Dynamic_Programming/DungeonGame.java
@@ -1,69 +1,43 @@
 package Comp_P.Dynamic_Programming;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
+// for just  reference video  https://leetcode.com/problems/dungeon-game/discuss/704381/Video-explanation
 class DungeonGameHandler
 {
     int data[][];
+    int storage[][];
     void input(int data[][])
     {
+
+        storage=new int[data.length][data[0].length];
+        for (int t[]:storage)
+            Arrays.fill(t,-1);
         this.data=data;
     }
-    int [] minimumHp(int i,int j)
+    int minimumHp(int i,int j)
     {
-        if (i==0&&j==0)
-        if (data[i][j]>0)
-                return new int[]{1,data[i][j]+1};
-            else
-                return new int[]{1-data[i][j],1};
-        else if (i<0||j<0)
-            return new int[]{-1,-1};
+        if(i==data.length-1&&j==data[0].length-1)
+            return storage[i][j]=Math.max(1,1-data[i][j]);
+        else if (i>=data.length||j>=data[0].length)
+            return -1;
+        if (storage[i][j]!=-1)
+            return storage[i][j];
+        int temp1= minimumHp(i+1,j);
+        int temp2=  minimumHp(i,j+1);
+        if (temp1==-1&&temp2!=-1)
+            return storage[i][j]=Math.max(1,temp2-data[i][j]);
+        else if (temp1!=-1&&temp2==-1)
+            return storage[i][j]=Math.max(1,temp1-data[i][j]);
         else
         {
-            int result[]=new int[2];
-            int result2[]=new int[2];
-           int temp1[]= minimumHp(i-1,j);
-           int temp2[]= minimumHp(i,j-1);
-           if (temp2[0]!=-1)
-           {
-               int value=temp2[1]+data[i][j];
-               if (value<=0)
-               {
-                   result[0]=temp2[0]+(1-value);
-                   result[1]=1;
-               }
-               else {
-                   result[0]=temp2[0];
-                   result[1] = value;
-               }
-           }
-           if (temp1[0]!=-1)
-           {
-               int value=temp1[1]+data[i][j];
-               if (value<=0)
-               {
-                   result2[0]=temp1[0]+(1-value);
-                   result2[1]=1;
-               }
-               else {
-                   result2[0]=temp1[0];
-                   result2[1] = value;
-               }
-           }
-           if (result[0]==0)
-               return result2;
-           else if (result2[0]==0)
-               return result;
-           else
-               return result[0]<result2[0]?result:result2;
-
+            int min=Math.min(temp1,temp2);
+            return storage[i][j]=Math.max(1,min-data[i][j]);
         }
+
     }
-
-
-
-
 }
+
 public class DungeonGame {
     public static void main(String[] args) {
         DungeonGameHandler handler =new DungeonGameHandler();
@@ -83,7 +57,7 @@ public class DungeonGame {
             }
         }
         handler.input(data);
-        int result[]=handler.minimumHp(data.length-1,data[0].length-1);
-        System.out.println("The Minimum Initial Hp should be "+result[0]);
+        int result=handler.minimumHp(0,0);
+        System.out.println("The Minimum Initial Hp should be "+result);
     }
 }
