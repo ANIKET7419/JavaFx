61ee63b56b7b9d56d6439f94305ea515ccb1b853
aniketranag123@gmail.com <aniketranag123@gmail.com>
2020-10-11 17:00:57 +0530

    Competetive Programming

--- src/Comp_P/Bridge.java
+++ src/Comp_P/Bridge.java
@@ -2,38 +2,42 @@ package Comp_P;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-class node
+class Node
 {
-    int min=Integer.MAX_VALUE;
+    int min;
     int value;
-    int time;
 }
 public class Bridge {
-    static ArrayList<Integer>[] graph;
+    static ArrayList<Node>[] graph;
     static ArrayList<int[]>result;
     static boolean isvisited[];
-    static void findBridge(int source) {
-
+    static Node nodes[];
+    static int time=0;
+    static void findBridge(int source,int parent) {
         isvisited[source]=true;
-        for(int i:graph[source])
+        nodes[source].min=time++;
+        for(Node i:graph[source])
         {
-            if(!isvisited[i])
-            {
-                findBridge(i);
-            }
-            else
-            {
-
-            }
+            if(!isvisited[i.value])
+                findBridge(i.value,source);
         }
-
-
+        for(Node i:graph[source]) {
+            if(parent!=-1&&nodes[parent].value!=i.value)
+            nodes[source].min = Math.min(nodes[source].min, i.min);
+        }
+        if(parent!=-1&&nodes[parent].min<nodes[source].min)
+            result.add(new int[]{source,parent});
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int e = scanner.nextInt();
         graph = new ArrayList[n];
+       nodes=new Node[n];
+        for(int i=0;i<n;i++) {
+            nodes[i] = new Node();
+            nodes[i].value=i;
+        }
         for (int i = 0; i < e; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
@@ -41,15 +45,14 @@ public class Bridge {
                 graph[a] = new ArrayList<>();
             if (graph[b] == null)
                 graph[b] = new ArrayList<>();
-            graph[a].add(b);
-            graph[b].add(a);
 
+            graph[a].add(nodes[b]);
+            graph[b].add(nodes[a]);
         }
-
-
-
-
-
+        isvisited=new boolean[n];
+        findBridge(0,-1);
+        System.out.println("Bridges Are : : -> ");
+        System.out.println(result);
 
     }
 }
