3cdfddebea613cd6e7ccc17a2af3bd437e998b6b
aniketranag123@gmail.com <aniketranag123@gmail.com>
2020-10-22 16:25:50 +0530

    Competetive Programming

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.chrono.Chronology;
+import java.util.Scanner;
 
 class b
 {
@@ -23,12 +24,17 @@ static  int m=100;
 }
 public class practice {
     public static void main(String[] args){
-     float f=Float.NEGATIVE_INFINITY;
+     /*float f=Float.NEGATIVE_INFINITY;
         System.out.println(f);
         Timestamp timestamp=new Timestamp(System.currentTimeMillis());
         LocalDateTime dat=timestamp.toLocalDateTime();
         System.out.println(dat.getHour());
         System.out.println("aniket".substring(0,1));
-
+*/
+        final int b;
+        b=2378374;
+        System.out.println(b);
+        char d1='o';
+       // char op=d1+598;
     }
 }
