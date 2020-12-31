d29bb887c0389671bc58651fbb08a8d8b17a767c
aniketranag123@gmail.com <aniketranag123@gmail.com>
2020-12-31 23:23:06 +0530

    Competetive Programming

--- src/Comp_P/_24Game.java
+++ src/Comp_P/_24Game.java
@@ -1,44 +1,74 @@
 package Comp_P;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class _24Game {
-    static ArrayList <Integer>cards;
     static char operator[]={'+','-','/','*'};
-    public static  boolean solve()
+    public static  boolean solve(double cards[])
     {
-        for (int i=0;i<4;i++)
+
+        if (cards.length==1)
         {
-            for(int j=i;j<4;j++)
+            return Math.abs(cards[0]-24)<0.0000001;
+        }
+        for (int i=0;i<cards.length;i++)
+        {
+            for(int j=i+1;j<cards.length;j++)
             {
-                int a=cards.get(i);
-                int b=cards.get(j);
-                cards.remove(i);
-                cards.remove(j);
+                double temp[]=new double[cards.length-1];
+                int w=0;
+                for (int k=0;k<cards.length;k++)
+                {
+                    if (k!=i&&k!=j)
+                    {
+                        temp[w++]=cards[k];
+                    }
+                }
                 for(char op:operator) {
-                    if (op=='+')
-                        cards.add(a+b);
-                    else if (op=='-')
-                        cards.add(a-b);
-                    else if (op=='*')
-                        cards.add(a*b);
-                    else
-                        cards.add(a/b);
-                    solve();
-                    cards.remove(cards.size()-1);
+                    if (op=='+') {
+                        temp[temp.length-1]=cards[i]+cards[j];
+                    }
+                    else if (op=='-') {
+                        temp[temp.length-1]=cards[i]-cards[j];
+                    }
+                    else if (op=='*') {
+                        temp[temp.length-1]=cards[i]*cards[j];
+                    }
+                    if(solve(temp))
+                        return true;
+                    if (op=='-')
+                    {
+
+                        temp[temp.length-1]=cards[j]-cards[i];
+                        if(solve(temp))
+                            return true;
+                    }
+                    if (op=='/')
+                    {
+                        if (cards[j]!=0) {
+                            temp[temp.length-1]=cards[i]/cards[j];
+                            if (solve(temp))
+                                return true;
+                        }
+                        if (cards[i]!=0)
+                        {
+                            temp[temp.length-1]=cards[j]/cards[i];
+                            if (solve(temp))
+                                return true;
+                        }
+                    }
+
                 }
             }
         }
-        return true;
+        return false;
     }
     public static void main(String[] args) {
-         cards=new ArrayList<>();
         System.out.println("Enter the card values from 1 to 9 ");
         Scanner scanner=new Scanner(System.in);
+        double cards[]=new double[4];
         for (int i=0;i<4;i++)
-            cards.add(scanner.nextInt());
-        solve();
+            cards[i]=scanner.nextDouble();
+       System.out.println("Is possible ? " +solve(cards));
 
     }
 }
