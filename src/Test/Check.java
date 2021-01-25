c4956fce24c0c5e3290d0f6574d659985785ddb8
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-01-25 23:16:45 +0530

    Competetive Programming

--- src/Test/Check.java
+++ src/Test/Check.java
@@ -1,29 +1,51 @@
 package Test;
 
+
+
+
+
 interface d1
 {
     default void hello()
     {
-        System.out.println("OP2");
+        System.out.println("d1");
     }
 }
-interface a1 extends d1
+interface d12 extends d1
 {
     default void hello()
     {
-        System.out.println("OP");
+        System.out.println("d12");
     }
 }
-interface c1 extends a1{
-
-}
-interface d2 extends c1{
-
-}
-interface b extends d1
+interface d3 extends d12
 {
+
 }
-class TestClass implements d2,b
+interface d4 extends d3
+{
+    default void hello()
+    {
+        System.out.println("d4");
+    }
+}
+interface d5 extends d1
+{
+
+}
+interface d7 extends d5{
+
+}
+interface d8 extends d6{
+
+}
+interface d6 extends d1
+{
+
+}
+
+
+class TestClass implements d7,d8,d6,d5,d4,d1,d12
 {
     public static void main(String args[])
     {
