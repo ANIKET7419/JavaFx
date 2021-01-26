efdc63d334903bc00b4837e77398c83ec811aa83
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-01-26 11:50:58 +0530

    Competetive Programming

--- src/Test/Check.java
+++ src/Test/Check.java
@@ -1,26 +1,41 @@
 package Test;
-interface  A2
+interface A1
 {
-     default void hello()
-    {
-        System.out.println("CLASS");
-    }
-}
-
-
- interface A1 extends A2
-{
-
     default void hello()
     {
-        System.out.println("INTERFACE ");
+        System.out.println("A1");
     }
 }
-interface A3 extends A1{
-
+interface A2 extends A1
+{
+    default void hello()
+    {
+        System.out.println("A2");
+    }
+}
+interface A3 extends A2
+{
+    default void hello()
+    {
+        System.out.println("A3");
+    }
+}
+interface A4 extends A3
+{
+    default void hello()
+    {
+        System.out.println("A4");
+    }
+}interface A5 extends A4
+{
+    default void hello()
+    {
+        System.out.println("A5");
+    }
 }
 
-class TestClass implements A3,A2
+
+class TestClass implements A5
 {
     public static void main(String args[])
     {
