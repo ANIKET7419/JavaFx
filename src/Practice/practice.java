d7faa75e47c88f728fe764cf32e745fd81e9c678
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-02-16 15:18:13 +0530

    Competetive Programming

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -1,12 +1,14 @@
+
+enum  okay{
+
+    void hello()
+    {
+
+    }
+    IO;
+}
 class Practice
 {
     public static void main(String[] args) {
-        switch(100){
-            case 11:{
-
-            }
-            break;
-
-        }
     }
 }
