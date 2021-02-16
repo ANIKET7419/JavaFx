525168ffe0cb3703c8db41d7f3a33a3e44c62e41
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-02-16 17:15:13 +0530

    Competetive Programming

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -1,36 +1,21 @@
-/*class op{
-    public int op111;
-       class op1{
+
+class a
+{
+     class D{
 
     }
 }
-class op2 extends op{
-    private int op111;
-   private class op1{
+class b extends a{
 
-    }
-
-}*/
-
-
-class Practice
+}
+ class Practice
 {
 
 
 
     public static void main(String[] args) {
-             op2 obj=new op2();
-             obj.hello();
 
-    }
-}
-class op{
-    static int op=9450;
-}
-class op2 extends  op{
-    static int op=904;
-    void hello()
-    {
-        System.out.print(this.op);
+   b obj=new b();
+   a.D inner=obj.new D();
     }
 }
