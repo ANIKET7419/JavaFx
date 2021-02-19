c2b20e1774835a4e77bf16499e2d7cecaa960c34
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-02-19 23:20:00 +0530

    Competetive Programming

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -1,21 +1,9 @@
-/**
 
- *okay bro.
-  *yes
-  *i am winner
-  *@author  ANiket
- */
 public class practice
 {
 
 
-    /**
 
-     <br>okay bro
-     yes
-     i am winner
-
-     */
     public static void main(String[] args) {
 
 
