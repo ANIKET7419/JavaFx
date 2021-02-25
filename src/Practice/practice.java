ac18d5fc25afb8067a62ec8fc09f2734b0d8703c
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-02-25 17:52:14 +0530

    Competetive Programming

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -1,11 +1,21 @@
 
-public class practice
-{
+class io {
 
 
 
-    public static void main(String[] args) {
 
+         class Okay1 {
+             private Okay1(){
+
+             }
+             private int i=10;
+            }
+
+            public void main1(String[] args) {
+                Okay1 op=new Okay1();
+                op.i=190;
+
+
+            }
 
     }
-}
